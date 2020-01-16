d12633a701e2b943f934d52bbe5353aea63d06ff
mirotic13 <50202852+mirotic13@users.noreply.github.com>
2020-01-16 13:35:48 +0100

    Altas conectadas

--- src/neocuentas/NeoCuentas.java
+++ src/neocuentas/NeoCuentas.java
@@ -6,7 +6,10 @@
 package neocuentas;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import metodos.Altas;
+import metodos.Menu;
 
 /**
  *
@@ -17,12 +20,29 @@ public class NeoCuentas {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader lee = new BufferedReader (new InputStreamReader (System.in));
         int op;
         do {
-            
-        }
+            op = Menu.main(lee);
+            switch (op) {
+                case 1:
+                    Altas.main(lee);
+                    break;
+                case 2:
+                    System.out.println("En construcción");
+                    break;
+                case 3:
+                    System.out.println("En construcción");
+                    break;
+                case 4:
+                    System.out.println("En construcción");
+                    break;
+                case 0:
+                    System.out.println("SALIENDO...");
+                    break;
+            }
+        }while(op != 0);
     }
     
 }
